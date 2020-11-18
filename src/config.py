52be5339e8d3855ab51a8bfd22e062fc2780a6f6
Carl Skeide <carl@skeide.se>
2020-11-18 01:30:28 +0000

    Make path prefix configurable

--- src/config.py
+++ src/config.py
@@ -12,9 +12,9 @@ CACHE_REDIS_URL = sec.load("redis_url", fallback="redis://localhost")
 CACHE_DEFAULT_TIMEOUT = 600
 
 # Flack
-FLACK_URL_PREFIX = "/"
 FLACK_DEFAULT_NAME = "GifRoulette"
 FLACK_TOKEN = sec.load("slack_token")
+FLACK_URL_PREFIX = sec.load("flack_prefix", fallback="/")
 
 # App
 GIPHY_API_KEY = sec.load("giphy_key")
